876afc860925e63b166b61d09a592d4575c0ec0a
capitanDANONE <128199326+capitanDANONE@users.noreply.github.com>
2025-04-11 15:16:06 +0300

    test

--- Assets/Scripts/SteamLobby.cs
+++ Assets/Scripts/SteamLobby.cs
@@ -10,11 +10,6 @@ public class LobbyManager : MonoBehaviour
 	private void DebugCreate() => CreateLobby();
 	private const int MaxLobbyMembers = 4; // Maximum number of players in the lobby
 
-	private Callback<LobbyCreated_t> LobbyCreated;
-	private Callback<LobbyEnter_t> LobbyEnter;
-	private Callback<LobbyChatUpdate_t> lobbyChatUpdate;
-	private Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
-
 	private void Awake()
 	{
 		if (!SteamManager.Initialized)
@@ -22,7 +17,7 @@ public class LobbyManager : MonoBehaviour
 			Debug.LogError("Steamworks is not initialized!");
 			return;
 		}
-		LobbyCreated = Callback<LobbyCreated_t>.Create(
+		Callback<LobbyCreated_t>.Create(
 		(LobbyCreated_t callback) =>
 			{
 				if (callback.m_eResult == EResult.k_EResultOK)
@@ -31,15 +26,15 @@ public class LobbyManager : MonoBehaviour
 					Debug.LogError($"Failed to create lobby. Error: {callback.m_eResult}");
 			}
 		);
-		LobbyEnter = Callback<LobbyEnter_t>.Create(
+		Callback<LobbyEnter_t>.Create(
 		(LobbyEnter_t callback) =>
 			Debug.Log($"Successfully entered lobby! Lobby ID: {callback.m_ulSteamIDLobby}")
 		);
-		lobbyChatUpdate = Callback<LobbyChatUpdate_t>.Create((LobbyChatUpdate_t callback) => {
+		Callback<LobbyChatUpdate_t>.Create((LobbyChatUpdate_t callback) => {
 			string action = callback.m_rgfChatMemberStateChange == 1 ? "joined" : "left";
 			Debug.Log($"{callback.m_ulSteamIDUserChanged} just {action}");
 		});
-		gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create((GameLobbyJoinRequested_t callback)=>{
+		Callback<GameLobbyJoinRequested_t>.Create((GameLobbyJoinRequested_t callback)=>{
 			SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
 		});
 	}
