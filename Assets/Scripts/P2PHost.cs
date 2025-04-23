392cc386087eea6b855034767709a05063ee7b87
capitanDANONE <128199326+capitanDANONE@users.noreply.github.com>
2025-04-23 21:52:24 +0300

    separating time

--- Assets/Scripts/P2PHost.cs
+++ Assets/Scripts/P2PHost.cs
@@ -23,62 +23,6 @@ public class P2PHost : P2PBase
         listenSocket = SteamNetworkingSockets.CreateListenSocketP2P(0, configuration.Length, configuration);
         Debug.Log("Listening for P2P connections");
     }
-	private void TryRecive(){
-        if (!isActive || connection == HSteamNetConnection.Invalid)
-            return;
-		
-        IntPtr[] messages = new IntPtr[10];
-        int numMessages = SteamNetworkingSockets.ReceiveMessagesOnConnection(connection, messages, messages.Length);
-        
-        if (numMessages > 0)
-			Debug.Log($"Received {numMessages} messages this frame");
-        for (int i = 0; i < numMessages; i++)
-        {
-            try {
-                SteamNetworkingMessage_t message = Marshal.PtrToStructure<SteamNetworkingMessage_t>(messages[i]);
-                byte[] data = new byte[message.m_cbSize];
-                Marshal.Copy(message.m_pData, data, 0, message.m_cbSize);
-				ProcesData(data, message);
-            } catch (Exception e) {
-                Debug.LogError($"Error processing message: {e}");
-            } finally {
-                SteamNetworkingMessage_t.Release(messages[i]);
-            }
-        }
-	}
-	void ProcesData(in byte[]data, SteamNetworkingMessage_t message) {	
-		EPackagePurpuse purpose = (EPackagePurpuse)data[0];
-		switch (purpose){
-			case EPackagePurpuse.Transform: 
-			{
-				P2PTransformPositionAndRotation transformMessage = new(data);
-				NetworkTransform cube = cubes[transformMessage.ID];
-				cube.transform.position = transformMessage.pos;
-				cube.transform.rotation = transformMessage.rot;
-				break;
-			} 
-			case EPackagePurpuse.TransformPosition: 
-			{
-				P2PTransformPosition transformPosition = new(data);
-				NetworkTransform cube = cubes[transformPosition.ID];
-				cube.transform.position = transformPosition.pos;
-				break;
-			}
-			case EPackagePurpuse.TransformRotation: 
-			{
-				P2PTransformRotation transformRotation = new(data);
-				NetworkTransform cube = cubes[transformRotation.ID];
-				cube.transform.rotation = transformRotation.rot;
-				break;
-			} 
-			default:
-			{
-				Debug.LogError("unsupported purpose");
-				break;
-			}
-		}
-	}
-    void Update() => TryRecive(); 
     void OnDestroy()
     {
         if (listenSocket != HSteamListenSocket.Invalid)
