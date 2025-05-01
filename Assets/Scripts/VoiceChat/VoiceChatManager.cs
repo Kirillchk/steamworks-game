10cce45d38e696bb3a3bebd9d90bc881c0834a8c
Barse4eck <semion09060s@gmail.com>
2025-05-01 22:17:38 +0300

    asfasf

--- Assets/Scripts/VoiceChat/VoiceChatManager.cs
+++ Assets/Scripts/VoiceChat/VoiceChatManager.cs
@@ -1,76 +1,35 @@
 using UnityEngine;
-using Steamworks;
+using Concentus;
 using System;
 public class VoiceChatManager : MonoBehaviour
 {
-    [SerializeField]private AudioSource audioSource;
-    private uint dataSize = 0;
-    private uint bytesWritten = 0;
-    private uint sampleRate;
-    public int check = 2;
-    [ContextMenu("Record Voice")]
-    private void Start()
+    int sampleRate = 48000;
+    int channels =1;
+    [SerializeField]int frequency = 22050;
+    string selectedDevice = "Микрофон (3- fifine Microphone)";
+    AudioClip audioClip;
+    [SerializeField]AudioSource audioSource;
+    public void Start()
     {
-        sampleRate = 48000;
-        SteamUser.StartVoiceRecording();
+        InvokeRepeating("Test1",0,1);
+        InvokeRepeating("Test2",1,1);
     }
-    [ContextMenu("Stop recording")]
-    private void StopRecording()
+    public void Test1()
     {
-        SteamUser.StopVoiceRecording();
+        audioClip = Microphone.Start(selectedDevice, false, 1, sampleRate);
     }
-    [ContextMenu("Get voice")]
-    private void Update()
+    void Test2()
     {
-        EVoiceResult voiceResult = SteamUser.GetAvailableVoice(out dataSize);
+        audioSource.clip = AudioClip.Create("test", audioClip.samples, 1, sampleRate, false);
+        float[] floatData = new float[audioClip.samples * channels];
 
-        if (voiceResult == EVoiceResult.k_EVoiceResultOK && dataSize > 0)
-        {
-            byte[] data = new byte[dataSize];
-            voiceResult = SteamUser.GetVoice(
-                true, //compressed?
-                data, //data 
-                dataSize,  //data size
-                out bytesWritten); // bytes count
+        audioClip.GetData(floatData,0);
+        audioSource.clip.SetData(floatData,0);
 
-            if (voiceResult == EVoiceResult.k_EVoiceResultOK && bytesWritten > 0)
-            {
-
-                uint decDataSize = sampleRate;
-                byte[] decData = new byte[decDataSize];
-                voiceResult = SteamUser.DecompressVoice(
-                    data,// data to decompress
-                    bytesWritten,// how many bytes data has
-                    decData,// data of decompressed data
-                    decDataSize,// size of decompressed data
-                    out uint decBytesWritten, // how many bytes in decompressed data
-                    sampleRate);
-                Debug.Log(voiceResult);
-
-                if (voiceResult == EVoiceResult.k_EVoiceResultOK && decBytesWritten > 0)
-                {
-                    int sampleCount = (int)decBytesWritten / check;
-                    float[] floatData = new float[sampleCount];
-
-                    for (int i = 0; i < sampleCount; i++)
-                    {
-                        short pcmSample = (short)((decData[i * 2 + 1] << 8) | decData[i * 2]); 
-                        floatData[i] = pcmSample / 32768.0f;
-                    }
-
-                    AudioClip clip = AudioClip.Create(
-                        "VoiceClip",
-                        sampleCount,
-                        1,
-                        (int)sampleRate,
-                        false
-                    );
-
-                    clip.SetData(floatData, 0);
-                    audioSource.PlayOneShot(clip);
-                    Debug.Log("Playing voice clip!");
-                }
-            }
-        }
+        audioSource.Play();
+    }
+    public void Update()
+    {
+         
     }
 }
