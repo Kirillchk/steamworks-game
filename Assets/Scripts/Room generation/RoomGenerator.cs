4c29401bd80bfa0f3e5b2cb46a5ba346ecc8f9d7
capitanDANONE <128199326+Kirillchk@users.noreply.github.com>
2025-05-16 05:02:54 +0300

    final without collision

--- Assets/Scripts/Room generation/RoomGenerator.cs
+++ Assets/Scripts/Room generation/RoomGenerator.cs
@@ -8,15 +8,18 @@ public class RoomGenerator : MonoBehaviour
 {
     private System.Random rng;
 	[SerializeField]List<GameObject> Rooms = new();
+	[SerializeField]float f = 180;
+	GameObject newRoom;
+	Vector3 randomDoor;
 	[ContextMenu("SpawnRooms")]
 	void spawnRoom(){
-		GameObject doorObject = GameObject.FindGameObjectsWithTag("Door")[0];
-		Vector3 randomDoor = doorObject.transform.position;
-		GameObject v = Instantiate(Rooms[rng.Next(0,2)], randomDoor, new Quaternion());
+		var doors = GameObject.FindGameObjectsWithTag("Door");
+		GameObject doorObject = doors[rng.Next(0, doors.Length)];
+		randomDoor = doorObject.transform.position;
+		newRoom = Instantiate(Rooms[rng.Next(0,2)], randomDoor, new Quaternion());
 		Destroy(doorObject);
-		Debug.DrawLine(randomDoor, randomDoor+Vector3.up, Color.green, float.MaxValue);
 
-		GameObject[] childrenWithTag = v.GetComponentsInChildren<Transform>(true)
+		GameObject[] childrenWithTag = newRoom.GetComponentsInChildren<Transform>(true)
 			.Where(child => child.CompareTag("Door"))
 			.Select(child => child.gameObject) // Convert Transform to GameObject
 			.ToArray();
@@ -25,29 +28,19 @@ public class RoomGenerator : MonoBehaviour
 		Destroy(secondDoorObject);
 		Vector3 secondDoor = secondDoorObject.transform.position;
 		Vector3 offset = secondDoor - randomDoor;
-		Debug.DrawLine(secondDoor, secondDoor + Vector3.up, Color.blue, float.MaxValue);
 
-		float ang = Vector3.SignedAngle(offset, Vector3.forward, Vector3.up);
-		Debug.DrawLine(new(), offset.normalized, Color.yellow, float.MaxValue);
-		Debug.DrawLine(new Vector3()+Vector3.up, Vector3.back + Vector3.up, Color.yellow, float.MaxValue);
-		
-		Debug.Log(ang);
-		v.transform.position -= offset;
-		
-		//if((ang >= 135 && ang <= 180)||(ang <= -135 && ang <= 180)){
-		//	v.transform.RotateAround(randomDoor, Vector3.up, 180);
-		//	v.transform.position -= offset;
-		//} else if((ang <= 45 && ang >= 0)||(ang >= -45 && ang <= 0)){
-		//	v.transform.RotateAround(randomDoor, Vector3.up, 0);
-		//	v.transform.position += offset;
-		//} else if(ang >= -135 && ang <= -45){
-		//	v.transform.position += offset;
-		//	v.transform.RotateAround(randomDoor, Vector3.up, -90);
-		//} else if(ang <= 135 && ang >= 45){
-		//	v.transform.position += offset;
-		//	v.transform.RotateAround(randomDoor, Vector3.up, 90);
-		//}
-
+		Debug.Log("new one: " +
+			Vector3.SignedAngle(offset, doorObject.transform.localPosition.normalized * -1, Vector3.up)
+			+ "already rotated" + doorObject.transform.parent.rotation.eulerAngles.y);
+		newRoom.transform.position -= offset;
+		float angle =
+			Vector3.SignedAngle(offset, doorObject.transform.localPosition.normalized * -1, Vector3.up)
+			- doorObject.transform.parent.rotation.eulerAngles.y;
+		newRoom.transform.RotateAround(randomDoor, Vector3.up, angle);
+	}
+	[ContextMenu("Rotate")]
+	void Rotate(){
+		newRoom.transform.RotateAround(randomDoor, Vector3.up, f);
 	}
 	void Start(){
 		rng = new(1);
