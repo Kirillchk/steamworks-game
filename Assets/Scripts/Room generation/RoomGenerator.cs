77fe1e8cb7e1f0e83d07435bab1cbac8b563abe2
Kirillchk <128199326+Kirillchk@users.noreply.github.com>
2025-06-04 21:03:04 +0300

    added collision detection

--- Assets/Scripts/Room generation/RoomGenerator.cs
+++ Assets/Scripts/Room generation/RoomGenerator.cs
@@ -2,48 +2,71 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
-    private System.Random rng;
-	[SerializeField]List<GameObject> Rooms = new();
-	[SerializeField]float f = 180;
+	private System.Random rng;
+	[SerializeField] List<GameObject> Rooms = new();
+	List<BoxCollider> RoomColliders = new();
 	GameObject newRoom;
 	Vector3 randomDoor;
 	[ContextMenu("SpawnRooms")]
-	void spawnRoom(){
+	void TryAddRoom(){
+		Debug.Log(spawnRoom()?"Room spawned":"Room fucked up");
+	}
+	bool spawnRoom()
+	{
+		// Selects random door inits new room and then destroys selected door
 		var doors = GameObject.FindGameObjectsWithTag("Door");
 		GameObject doorObject = doors[rng.Next(0, doors.Length)];
 		randomDoor = doorObject.transform.position;
-		newRoom = Instantiate(Rooms[rng.Next(0,2)], randomDoor, new Quaternion());
+		newRoom = Instantiate(Rooms[rng.Next(0, 2)], randomDoor, new Quaternion());
 		Destroy(doorObject);
-
+		// Gets random door of new room and destroys it
 		GameObject[] childrenWithTag = newRoom.GetComponentsInChildren<Transform>(true)
 			.Where(child => child.CompareTag("Door"))
-			.Select(child => child.gameObject) // Convert Transform to GameObject
+			.Select(child => child.gameObject)
 			.ToArray();
-		
-		GameObject secondDoorObject = childrenWithTag[rng.Next(0,childrenWithTag.Length)];
+		GameObject secondDoorObject = childrenWithTag[rng.Next(0, childrenWithTag.Length)];
 		Destroy(secondDoorObject);
+		// Applies calculated rotation angle to align selected doors
 		Vector3 secondDoor = secondDoorObject.transform.position;
 		Vector3 offset = secondDoor - randomDoor;
-
-		Debug.Log("new one: " +
-			Vector3.SignedAngle(offset, doorObject.transform.localPosition.normalized * -1, Vector3.up)
-			+ "already rotated" + doorObject.transform.parent.rotation.eulerAngles.y);
 		newRoom.transform.position -= offset;
 		float angle =
 			Vector3.SignedAngle(offset, doorObject.transform.localPosition.normalized * -1, Vector3.up)
 			- doorObject.transform.parent.rotation.eulerAngles.y;
 		newRoom.transform.RotateAround(randomDoor, Vector3.up, angle);
+		// Check for overlaping
+		var comp = newRoom.GetComponent<BoxCollider>();
+		Physics.SyncTransforms();
+		bool flag = false;
+		foreach (var col in RoomColliders) {
+			if (comp.bounds.Intersects(col.bounds))
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (flag) {
+			Destroy(newRoom);
+			return false;
+		}
+		else {
+			RoomColliders.Add(comp);
+			return true;
+		}
 	}
-	[ContextMenu("Rotate")]
-	void Rotate(){
-		newRoom.transform.RotateAround(randomDoor, Vector3.up, f);
-	}
-	void Start(){
+	void Start()
+	{
 		rng = new(1);
 		Instantiate(Rooms[0], new Vector3(), new Quaternion());
+		for (int i = 0; i<8; ) {
+			if (spawnRoom())
+				i++;
+			
+		}
 	}
 }
