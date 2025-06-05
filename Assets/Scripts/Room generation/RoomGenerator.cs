d083256690a47c29df3161782c779ab4586b2bca
Kirillchk <128199326+Kirillchk@users.noreply.github.com>
2025-06-05 19:37:21 +0300

    fixed overlaping but in a very bad way

--- Assets/Scripts/Room generation/RoomGenerator.cs
+++ Assets/Scripts/Room generation/RoomGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -12,9 +13,14 @@ public class RoomGenerator : MonoBehaviour
 	List<BoxCollider> RoomColliders = new();
 	GameObject newRoom;
 	Vector3 randomDoor;
+	int ind = 0;
 	[ContextMenu("SpawnRooms")]
-	void TryAddRoom(){
-		Debug.Log(spawnRoom()?"Room spawned":"Room fucked up");
+	void TryAddRoom()
+	{
+		var res = spawnRoom();
+		Debug.Log($"sucsess: {res} ind: {ind}");
+		if (res)
+			ind++;
 	}
 	bool spawnRoom()
 	{
@@ -40,12 +46,14 @@ public class RoomGenerator : MonoBehaviour
 			- doorObject.transform.parent.rotation.eulerAngles.y;
 		newRoom.transform.RotateAround(randomDoor, Vector3.up, angle);
 		// Check for overlaping
+		// TODO: orgonize maybe
 		var comp = newRoom.GetComponent<BoxCollider>();
 		Physics.SyncTransforms();
 		bool flag = false;
 		foreach (var col in RoomColliders) {
 			if (comp.bounds.Intersects(col.bounds))
 			{
+				Debug.Log($"{comp.bounds} intersects {col.bounds}");
 				flag = true;
 				break;
 			}
@@ -56,17 +64,23 @@ public class RoomGenerator : MonoBehaviour
 		}
 		else {
 			RoomColliders.Add(comp);
+			Debug.Log($"{comp.bounds} added to list Room {newRoom.transform.position}");
 			return true;
 		}
 	}
-	void Start()
+	async void Start()
 	{
 		rng = new(1);
 		Instantiate(Rooms[0], new Vector3(), new Quaternion());
-		for (int i = 0; i<8; ) {
-			if (spawnRoom())
-				i++;
-			
+		while (ind < 8)
+		{
+			var res = spawnRoom();
+			Debug.Log($"sucsess: {res} ind: {ind}");
+			if (res)
+				ind++;
+			// TODO: FIX THIS for faster scene loading
+			// bullshit solution for rooms overlaping if there is no delay
+			await Task.Delay(100); 
 		}
 	}
 }
