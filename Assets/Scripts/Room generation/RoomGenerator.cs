10a5ff7b734bd26e849beefc54cfedafcfbcba0d
Kirillchk <128199326+Kirillchk@users.noreply.github.com>
2025-06-07 18:44:23 +0300

    kinda fixed rotation

--- Assets/Scripts/Room generation/RoomGenerator.cs
+++ Assets/Scripts/Room generation/RoomGenerator.cs
@@ -12,7 +12,7 @@ public class RoomGenerator : MonoBehaviour
 	[SerializeField] List<GameObject> Rooms = new();
 	List<BoxCollider> RoomColliders = new();
 	GameObject newRoom;
-	Vector3 randomDoor;
+	Vector3 firstDoorPosition;
 	int ind = 0;
 	[ContextMenu("SpawnRooms")]
 	void TryAddRoom()
@@ -26,10 +26,10 @@ public class RoomGenerator : MonoBehaviour
 	{
 		// Selects random door inits new room and then destroys selected door
 		var doors = GameObject.FindGameObjectsWithTag("Door");
-		GameObject doorObject = doors[rng.Next(0, doors.Length)];
-		randomDoor = doorObject.transform.position;
-		newRoom = Instantiate(Rooms[rng.Next(0, 2)], randomDoor, new Quaternion());
-		Destroy(doorObject);
+		GameObject firstdoorObject = doors[rng.Next(0, doors.Length)];
+		firstDoorPosition = firstdoorObject.transform.position;
+		newRoom = Instantiate(Rooms[rng.Next(0, 2)], firstDoorPosition, new Quaternion());
+		Destroy(firstdoorObject);
 		// Gets random door of new room and destroys it
 		GameObject[] childrenWithTag = newRoom.GetComponentsInChildren<Transform>(true)
 			.Where(child => child.CompareTag("Door"))
@@ -37,14 +37,21 @@ public class RoomGenerator : MonoBehaviour
 			.ToArray();
 		GameObject secondDoorObject = childrenWithTag[rng.Next(0, childrenWithTag.Length)];
 		Destroy(secondDoorObject);
-		// Applies calculated rotation angle to align selected doors
-		Vector3 secondDoor = secondDoorObject.transform.position;
-		Vector3 offset = secondDoor - randomDoor;
+		//smashes doors together
+		Vector3 secondDoorPosition = secondDoorObject.transform.position;
+		Vector3 offset = secondDoorPosition - firstDoorPosition;
 		newRoom.transform.position -= offset;
+		// Applies calculated rotation angle to align selected doors
 		float angle =
-			Vector3.SignedAngle(offset, doorObject.transform.localPosition.normalized * -1, Vector3.up)
-			- doorObject.transform.parent.rotation.eulerAngles.y;
-		newRoom.transform.RotateAround(randomDoor, Vector3.up, angle);
+			Vector3.SignedAngle(SnapToCardinal(offset), SnapToCardinal(firstdoorObject.transform.localPosition) * -1, Vector3.up)
+			- firstdoorObject.transform.parent.rotation.eulerAngles.y;
+
+		Debug.DrawLine(new(), offset, Color.red, 10);
+		Debug.DrawLine(new(0, 1), firstdoorObject.transform.localPosition * -1, Color.green, 10);
+
+		Debug.Log(angle + $" vars: ofs:{offset} dorobj:{firstdoorObject.transform.localPosition * -1}");
+		newRoom.transform.RotateAround(firstDoorPosition, Vector3.up, angle);
+
 		// Check for overlaping
 		// TODO: orgonize maybe
 		var comp = newRoom.GetComponent<BoxCollider>();
@@ -70,8 +77,10 @@ public class RoomGenerator : MonoBehaviour
 	}
 	async void Start()
 	{
-		rng = new(1);
-		Instantiate(Rooms[0], new Vector3(), new Quaternion());
+		rng = new(2);
+		RoomColliders.Add(
+			Instantiate(Rooms[0], new Vector3(), new Quaternion()).GetComponent<BoxCollider>()
+		);
 		//while (ind < 8)
 		//{
 		//	var res = spawnRoom();
@@ -82,5 +91,27 @@ public class RoomGenerator : MonoBehaviour
 		//	// bullshit solution for rooms overlaping if there is no delay
 		//	await Task.Delay(100); 
 		//}
+	}
+	Vector3 SnapToCardinal(Vector3 input)
+	{
+		Vector3 rounded = new Vector3(
+			Mathf.Round(input.x),
+			Mathf.Round(input.y),
+			Mathf.Round(input.z)
+		);
+
+		// Find the axis with the largest absolute value
+		float max = Mathf.Max(
+			Mathf.Abs(rounded.x),
+			Mathf.Abs(rounded.y),
+			Mathf.Abs(rounded.z)
+		);
+
+		// Zero out non-dominant axes
+		if (Mathf.Abs(rounded.x) != max) rounded.x = 0;
+		if (Mathf.Abs(rounded.y) != max) rounded.y = 0;
+		if (Mathf.Abs(rounded.z) != max) rounded.z = 0;
+
+		return rounded.normalized;
 	}
 }
