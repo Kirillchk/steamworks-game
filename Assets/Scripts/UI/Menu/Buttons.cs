bdd303dd0d389899e654b3de54b17f15903a1343
Barse4eck <semion09060s@gmail.com>
2025-06-22 04:56:51 +0300

    fully working mic + compressing

--- Assets/Scripts/UI/Menu/Buttons.cs
+++ Assets/Scripts/UI/Menu/Buttons.cs
@@ -3,6 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    public void QuitTheGame()=>Application.Quit();
-    public void Solo()=>SceneManager.LoadScene("GameScene");    
+    GameObject menu;
+    GameObject options;
+    GameObject mic;
+    GameObject scroll;
+    void Start()
+    {
+        menu = transform.GetChild(1).gameObject;
+        options = transform.GetChild(2).gameObject;
+        mic = transform.GetChild(2).GetChild(0).gameObject;
+        scroll = mic.transform.GetChild(1).gameObject;
+    }
+    public void QuitTheGame() => Application.Quit();
+    public void Solo() => SceneManager.LoadScene("GameScene");
+    public void Settings()
+    {
+        menu.SetActive(false);
+        options.SetActive(true);
+    }
+    public void Back()
+    {
+        menu.SetActive(true);
+        options.SetActive(false);
+    }
+    public void Mic()
+    {
+        if (scroll.activeSelf == false)
+        {
+            scroll.SetActive(true);
+        }
+        else
+        {
+            scroll.SetActive(false);
+        }
+    }
 }
