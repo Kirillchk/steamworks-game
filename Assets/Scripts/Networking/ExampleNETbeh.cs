b56e22ebf8c2b45ddb4311f9d059615c7e2ed731
Kirillchk <128199326+Kirillchk@users.noreply.github.com>
2025-06-26 12:34:00 +0300

    kinda final

--- Assets/Scripts/Networking/ExampleNETbeh.cs
+++ Assets/Scripts/Networking/ExampleNETbeh.cs
@@ -1,30 +1,34 @@
 using System;
 using UnityEngine;
-
+using static NetworkSyncExtensions; 
 public class ExampleNETbeh : NetworkActions
 {
-	[ContextMenu("Test1")]
-	void SUS()
-	{
-		TriggerSync(new Action<int, int>(Disentary), 1, 2);
-	}
-	[ContextMenu("Test2")]
-	void sus()
-	{
-		TriggerSync(new Action(AFAF));
-	}
-	[ContextMenu("Test3")]
-	void sosat()
-	{
-		TriggerSync(new Action(AFAF));
-		TriggerSync(new Action<int, int>(Disentary), 1, 2);
-		TriggerSync(new Action<int, int>(Disentary), 1, 2);
-		TriggerSync(new Action(AFAF));
-	}
-	[CanTriggerSync]
-	public void Disentary(int num1, int num2)
-		=> Debug.Log($"num1{num1} num2{num2}");
-	[CanTriggerSync]
-	public void AFAF()
-		=> Debug.Log($"triggered nahui");
+    [ContextMenu("Test1")]
+    void SUS()
+    {
+        this.Sync(Disentary, 1, 2);
+    }
+    
+    [ContextMenu("Test2")]
+    void sus()
+    {
+        this.Sync(AFAF);
+    }
+    
+    [ContextMenu("Test3")]
+    void sosat()
+    {
+        this.Sync(AFAF);
+        this.Sync(Disentary, 1, 2);
+        this.Sync(Disentary, 1, 2);
+        this.Sync(AFAF);
+    }
+    
+    [CanTriggerSync]
+    public void Disentary(int num1, int num2)
+        => Debug.Log($"num1{num1} num2{num2}");
+        
+    [CanTriggerSync]
+    public void AFAF()
+        => Debug.Log($"triggered nahui");
 }
--- Assets/Scripts/Networking/NetworkActions.cs
+++ Assets/Scripts/Networking/NetworkActions.cs
@@ -10,8 +10,7 @@ using MessagePack;
 
 public class NetworkActions : MonoBehaviour
 {
-	// TODO: AAAAAAAAAAAAAAAAAAAAAA wtf is this piss
-    Vector3 ID;
+	Vector3 ID;
 	protected List<Delegate> delegates = new();
 	async void Awake()
 	{
@@ -20,7 +19,7 @@ public class NetworkActions : MonoBehaviour
 		ID = GetComponent<NetworkIdentity>().uniqueVector;
 
 		P2PBase.networkActionScripts[ID] = this;
-		
+
 		var methods = GetType().GetMethods(BindingFlags.Public | BindingFlags.Instance)
 			.Where(m => m.GetCustomAttributes(typeof(CanTriggerSync), false).Length > 0);
 		foreach (var method in methods)
@@ -32,7 +31,6 @@ public class NetworkActions : MonoBehaviour
 					));
 		Debug.Log($"{methods.Count()}, {ID}");
 	}
-	// wraper
 	internal void TriggerSync(in Delegate del, params object[] args)
 	{
 		del.DynamicInvoke(args);
@@ -44,12 +42,31 @@ public class NetworkActions : MonoBehaviour
 				Index = delegates.IndexOf(del),
 				Length = data.Length,
 				Args = data
-			}.GetBinary()
+			}.GetBinary() // maybe remove
 		);
 	}
-	// for invoking method after package
 	internal void InvokeFromBytes(in int ind, in byte[] data) =>
 		delegates[ind].DynamicInvoke(MessagePackSerializer.Deserialize<object[]>(data));
 	[AttributeUsage(AttributeTargets.Method)]
 	protected class CanTriggerSync : Attribute { }
+}
+public static class NetworkSyncExtensions
+{
+    public static void Sync(this NetworkActions na, Action action)
+        => na.TriggerSync(action);
+    
+    public static void Sync<T>(this NetworkActions na, Action<T> action)
+        => na.TriggerSync(action);
+    
+    public static void Sync<T1, T2>(this NetworkActions na, Action<T1, T2> action, T1 arg1, T2 arg2)
+        => na.TriggerSync(action, arg1, arg2);
+    
+    public static void Sync<T1, T2, T3>(this NetworkActions na, Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
+        => na.TriggerSync(action, arg1, arg2, arg3);
+    
+    public static void Sync<T1, T2, T3, T4>(this NetworkActions na, Action<T1, T2, T3, T4> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        => na.TriggerSync(action, arg1, arg2, arg3, arg4);
+    
+    public static void Sync<T1, T2, T3, T4, T5>(this NetworkActions na, Action<T1, T2, T3, T4, T5> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        => na.TriggerSync(action, arg1, arg2, arg3, arg4, arg5);
 }
