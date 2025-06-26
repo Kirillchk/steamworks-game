1ea7ac92a5fd39fe1db02d132ee7d4fe62a3e45e
Barse4eck <semion09060s@gmail.com>
2025-06-26 11:33:33 +0300

    gasgasgasg

--- Assets/Scripts/VoiceChat/Mic/MicAudioSource.cs
+++ Assets/Scripts/VoiceChat/Mic/MicAudioSource.cs
@@ -90,10 +90,10 @@ namespace Adrenak.UniMic
         }
         void RecieveFrame(AudioFrame audioFrame)
         {
+            Debug.Log("AAAAAAAAAASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
             decodedAudio = decoder.Run(audioFrame);
             float[] audio = Utils.Bytes.BytesToFloats(decodedAudio.samples);
             StreamedAudioSource.Feed(decodedAudio.frequency, decodedAudio.channelCount, audio);
-            Debug.Log(audio);
         }
 
         void OnStopRecording()
