cbfc22899a0476d882a6e7c87a8a4d036334707f
Barse4eck <semion09060s@gmail.com>
2025-06-30 21:14:39 +0300

    fasafsafs

--- Assets/Scripts/Networking/NetworkIdentity.cs
+++ Assets/Scripts/Networking/NetworkIdentity.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class NetworkIdentity : MonoBehaviour
 {
 	internal Vector3 uniqueVector;
-	internal bool isOwner;
+	internal bool isOwner = false;
 	void Start() =>
 		uniqueVector = transform.position;
 }
