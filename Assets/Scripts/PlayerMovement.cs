57138a94f2d0389c2ab676a7b1297ce6f0c1c66b
Barse4eck <semion09060s@gmail.com>
2025-08-03 06:07:21 +0300

    faster jump and better wasd speed limit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,15 @@
-using System;
-using System.Collections;
-using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
-	public float jumpForce = .6f,
-		walkSpeed = 7, runMultiplier = 2, acceler = 2,
-		groundDamping = 5, airDamping = 5, penalty = 1f,
-		timeToLimit = 1;
+	public float jumpForce = 8,
+		walkSpeed = 10, runMultiplier = 2, acceler,
+		daming = 5, penalty = 1, gravityScale = 3,
+		rayCastLengh = 1.1f, magnitude, g = -9.81f,
+		speedLimit, drag;
 	float rotationX = 0f;
-	public Vector3 move, wishDir, transVec, velToAdd = Vector3.zero, velFromWASD, vel;
-
-	public ForceMode forceMode = ForceMode.Acceleration;
-	public float g = -9.81f;
-	[SerializeField] bool isGrounded;
+	public Vector3 move, wishDir, vel;
+	public bool isGrounded, jump;
 	internal Transform playerCamera;
 	Rigidbody rb;
 	void Start()
@@ -27,7 +21,15 @@ public class PlayerMovement : MonoBehaviour
 	}
 	void Update()
 	{
-		// Handle mouse look (for rotating camera)
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+			jump = true;
+		speedLimit = Input.GetKey(KeyCode.LeftShift) ?
+			isGrounded ? walkSpeed * runMultiplier : walkSpeed / penalty * runMultiplier
+			: isGrounded ? walkSpeed : walkSpeed / penalty;
+	}
+    void FixedUpdate()
+    {
+        		// Handle mouse look (for rotating camera)
 		float mouseX = Input.GetAxis("Mouse X");
 		float mouseY = Input.GetAxis("Mouse Y");
 
@@ -35,46 +37,68 @@ public class PlayerMovement : MonoBehaviour
 		rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 		playerCamera.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
 		transform.Rotate(Vector3.up * mouseX);
+
 		isGrounded = Physics.Raycast(
 			transform.position,
 			Vector3.down,
-			1.1f,
+			rayCastLengh,
 			LayerMask.GetMask("Default")
 		);
-		if (isGrounded)
-			Physics.gravity = Vector3.up * -1;
-		if (!isGrounded)
-			Physics.gravity = Vector3.up * -9.81f;
-		g=Physics.gravity.y;
-		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-			rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
 
-		// rb.linearDamping = isGrounded ? groundDamping : airDamping;
+		g = -9.81f * gravityScale;
+		Physics.gravity = Vector3.up * g;
+		if (!isGrounded)
+			jump = false;
+		if (jump)
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+		drag = Mathf.Clamp01(1f - daming * Time.deltaTime);
 
 		move = new Vector3(
-				Input.GetAxisRaw("Horizontal"),
-				0,
-				Input.GetAxisRaw("Vertical")
-			).normalized;
+			Input.GetAxisRaw("Horizontal"),
+			0,
+			Input.GetAxisRaw("Vertical")
+		).normalized;
+		acceler = speedLimit / (drag * 10);
 		wishDir = //wasd fix
 			(transform.right * move.x +
-			transform.forward * move.z).normalized;
-
-		var speedLimit = Input.GetKey(KeyCode.LeftShift) ?
-		isGrounded ? walkSpeed * runMultiplier : walkSpeed / penalty * runMultiplier
-		: isGrounded ? walkSpeed : walkSpeed / penalty;
-
-		if ((rb.linearVelocity + wishDir).magnitude <= speedLimit)
-			rb.AddForce(wishDir, ForceMode.VelocityChange);
-		float drag = Mathf.Clamp01(1f - groundDamping * Time.deltaTime);
+			transform.forward * move.z).normalized * acceler;
+			
+		if (new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z).magnitude < speedLimit && move!=Vector3.zero)
+		{
+			if ((new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z) + wishDir).magnitude < speedLimit)
+			{
+				rb.AddForce(wishDir, ForceMode.VelocityChange);
+			}
+			else
+			{
+				//add speed exactly to limit
+				float root = Mathf.Sqrt(wishDir.x * wishDir.x + wishDir.z * wishDir.z);
+				if (root != 0)
+				{
+					float mult = (speedLimit / drag - new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z).magnitude) / root;
+					rb.AddForce(wishDir * mult, ForceMode.VelocityChange);
+				}
+			}
+		}
+		else
+		{
+			//wasd keeps a litle speed after limit
+			float root = Mathf.Sqrt(wishDir.x * wishDir.x + wishDir.z * wishDir.z);
+			if (drag != 0 && root != 0)
+			{
+				float dif = speedLimit / drag - speedLimit;
+				float mult = dif / root;
+				rb.AddForce(wishDir * mult, ForceMode.VelocityChange);
+			}
+		}
+		
 		rb.linearVelocity = new Vector3(rb.linearVelocity.x * drag, rb.linearVelocity.y, rb.linearVelocity.z * drag);
 
-		// rb.linearVelocity = rb.linearVelocity-
 		vel = rb.linearVelocity;
-		transVec = transform.forward + transform.right;
-		
-	}
-	private void OnDrawGizmos()
+		magnitude = (float)System.Math.Round(rb.linearVelocity.magnitude, 2);
+    }
+    private void OnDrawGizmos()
 	{
 		if (rb != null)
 		{
@@ -82,14 +106,6 @@ public class PlayerMovement : MonoBehaviour
 			Vector3 endPos = transform.position + rb.linearVelocity;
 			Gizmos.color = Color.red;
 			Gizmos.DrawLine(startPos, endPos);
-			// Debug.Log(rb.linearVelocity);
-			// float rad = Mathf.Deg2Rad * 30;
-			// Vector3 vec3 = new Vector3(
-			// 	Mathf.Cos(rad) * endPos.x + Mathf.Sin(rad) * endPos.x,
-			// 	1 * endPos.y,
-			// 	-Mathf.Sin(rad) * endPos.z + Mathf.Cos(rad) * endPos.z);
-			// Gizmos.DrawLine(endPos, vec3);
-			
 		}
 
 	}
