d07c98de6912b287c388aed4634ff3dd39f715f1
Kirillchk <128199326+Kirillchk@users.noreply.github.com>
2025-08-04 13:32:24 +0300

    added autoset

--- Assets/Scripts/Networking/NetworkIdentity.cs
+++ Assets/Scripts/Networking/NetworkIdentity.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 public class NetworkIdentity : MonoBehaviour
 {
 	internal Vector3 uniqueVector;
+	public bool autoset = true;
 	public bool isOwner = false;
-	void Start() {
+	void Start()
+	{
 		uniqueVector = transform.position;
+		if (autoset)
+			isOwner = P2PBase.isHost;
 	}
 }
