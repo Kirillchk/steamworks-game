e384415528eae33193681e3be59703ea89e57746
Barse4eck <semion09060s@gmail.com>
2025-08-04 08:05:55 +0300

    no friction in the air and wasd adds less speed in the air

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
 		speedLimit, drag;
 	public bool isGrounded, jump;
 	float rotationX = 0f, mouseX, mouseY;
-	Vector3 move;
+	public Vector3 move, wishDir, vel;
 	Rigidbody rb;
 	void Start()
 	{
@@ -32,9 +32,9 @@ public class PlayerMovement : MonoBehaviour
 		mouseX = Input.GetAxis("Mouse X") * 2;
 		mouseY = Input.GetAxis("Mouse Y") * 2;
 	}
-    void FixedUpdate()
-    {
-        // Handle mouse look (for rotating camera)
+	void FixedUpdate()
+	{
+		// Handle mouse look (for rotating camera)
 		rotationX -= mouseY;
 		rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 		Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
@@ -51,38 +51,50 @@ public class PlayerMovement : MonoBehaviour
 		Physics.gravity = Vector3.up * g;
 		if (!isGrounded)
 			jump = false;
+	
 		if (jump)
 			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
 		drag = Mathf.Clamp01(1f - daming * Time.deltaTime);
 
 		acceler = speedLimit / (drag * 10);
-		var wishDir = //wasd fix
+		wishDir = //wasd fix
 			(transform.right * move.x +
 			transform.forward * move.z).normalized * acceler;
 		var XZSpeed = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
 		float root = wishDir.magnitude;
+
+		
 		if (XZSpeed.magnitude < speedLimit && move != Vector3.zero)
 		{
 			if ((XZSpeed + wishDir).magnitude < speedLimit)
-				rb.AddForce(wishDir, ForceMode.VelocityChange);
+				rb.AddForce(isGrounded ? wishDir : wishDir / penalty, ForceMode.VelocityChange);
 			else if (root != 0)
 			{
 				//add speed exactly to limit
 				float mult = (speedLimit / drag - XZSpeed.magnitude) / root;
-				rb.AddForce(wishDir * mult, ForceMode.VelocityChange);
+				rb.AddForce(isGrounded ? wishDir * mult : wishDir * mult / penalty, ForceMode.VelocityChange);
 			}
 		}
-		else if (drag != 0 && root != 0)
+		else if (XZSpeed.normalized != wishDir.normalized && wishDir != Vector3.zero)
+		{
+			var velToAdd = wishDir.normalized - XZSpeed.normalized;
+			rb.AddForce(isGrounded ? velToAdd : velToAdd / penalty, ForceMode.VelocityChange);
+		}
+		else if (drag != 0 && root != 0 && isGrounded)
 		{
 			//wasd keeps a litle speed after limit
 			float dif = speedLimit / drag - speedLimit;
 			float mult = dif / root;
-			rb.AddForce(wishDir * mult, ForceMode.VelocityChange);
+			rb.AddForce(isGrounded ? wishDir * mult : wishDir * mult / penalty, ForceMode.VelocityChange);
 		}
-		
-		rb.linearVelocity = new (rb.linearVelocity.x * drag, rb.linearVelocity.y, rb.linearVelocity.z * drag);
+		if (!isGrounded)
+			drag = 1;
+
+		rb.linearVelocity = new(rb.linearVelocity.x * drag, rb.linearVelocity.y, rb.linearVelocity.z * drag);
 		magnitude = (float)System.Math.Round(rb.linearVelocity.magnitude, 2);
+		vel = new Vector3((float)System.Math.Round(rb.linearVelocity.x,2), (float)System.Math.Round(rb.linearVelocity.y,2),
+		(float)System.Math.Round(rb.linearVelocity.z,2));
     }
     private void OnDrawGizmos()
 	{
