6192591eb0e8fd7d0671a133de4685fb27edf484
Kirillchk <128199326+Kirillchk@users.noreply.github.com>
2025-08-07 12:21:41 +0300

    added functionality for vents

--- Assets/Scenes/lvl_facility/Rooms/prefabs/EnemyBehaviour.cs
+++ Assets/Scenes/lvl_facility/Rooms/prefabs/EnemyBehaviour.cs
@@ -1,3 +1,5 @@
+using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
@@ -10,13 +12,18 @@ public class EnemyBehaviour : MonoBehaviour
 	{
 		agent.SetDestination(Target.position);
 	}
-	void Update()
+	void FixedUpdate()
 	{
-		if (!agent.isOnOffMeshLink)
-			return;
+		if (agent.isOnOffMeshLink) Vent();
+	}
+	void Vent()
+	{
+		//TODO: fuck in the ass creator of unitys documentation
+		//everything is either depricated or described with a single sentance
+		//why the fuck is link data still called offmesh if its depricated?????
+		var area = agent.currentOffMeshLinkData.owner.GetComponent<NavMeshLink>().area;
+		if (area!= NavMesh.GetAreaFromName("Vent")) return;
 		agent.Warp(agent.currentOffMeshLinkData.endPos);
-		agent.CompleteOffMeshLink();
 		agent.SetDestination(Target.position);
-		agent.isStopped = false;
 	}
 }
